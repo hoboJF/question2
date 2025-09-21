215830ecca13b51077bedfa5d129d67f59e6d8c0
hoboJF <jadenfiero@gmail.com>
2025-09-21 03:49:51 -0400

    finished rectangle struct

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,36 @@
+struct Rectangle {
+    width: f64,
+    height: f64,
+}
+
+impl Rectangle {
+    fn new(width: f64, height: f64) -> Rectangle {
+        Rectangle { width, height }
+    }
+
+    fn area(&self) -> f64 {
+        self.width * self.height
+    }
+
+    fn perimeter(&self) -> f64 {
+        2.0*self.width + 2.0*self.height
+    }
+
+    fn is_square(&self) -> bool {
+        if self.width == self.height{
+            return true;
+        } else{
+            return false;
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let rect = Rectangle::new(10.0, 5.0);
+    println!("Area: {}", rect.area());
+    println!("Perimeter: {}", rect.perimeter());
+    println!("Is square? {}", rect.is_square());
+
+    assert!(Rectangle::new(5.0, 5.0).is_square());
+    assert!(!Rectangle::new(5.0, 6.0).is_square());
 }
